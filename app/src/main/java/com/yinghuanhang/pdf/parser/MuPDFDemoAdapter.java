148705b3abb3e2ad5852ff14ced8a6ea3623ff49
HangCao <yinghuanhang@sina.com>
2018-01-02 16:20:36 +0800

    添加演示Demo

--- app/src/main/java/com/yinghuanhang/pdf/parser/MuPDFDemoAdapter.java
+++ app/src/main/java/com/yinghuanhang/pdf/parser/MuPDFDemoAdapter.java
@@ -1,8 +1,14 @@
 package com.yinghuanhang.pdf.parser;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +16,7 @@ import java.util.List;
  * Created by Cao-Human on 2018/1/2
  */
 
-public class MuPDFDemoAdapter extends RecyclerView.Adapter {
+public class MuPDFDemoAdapter extends RecyclerView.Adapter<MuPDFDemoAdapter.MuPDFDemoHolder> {
     private List<String> mImages = new ArrayList<>();
 
     public void insert(String image) {
@@ -19,17 +25,29 @@ public class MuPDFDemoAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public MuPDFDemoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ImageView view = (ImageView) inflater.inflate(R.layout.adapter_choose_image_item, parent, false);
+        return new MuPDFDemoHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(MuPDFDemoHolder holder, int position) {
+        File file = new File(mImages.get(position));
+        Glide.with(holder.mImage.getContext()).load(file).diskCacheStrategy(DiskCacheStrategy.NONE).into(holder.mImage);
     }
 
     @Override
     public int getItemCount() {
         return mImages.size();
+    }
+
+    public static class MuPDFDemoHolder extends RecyclerView.ViewHolder {
+        public MuPDFDemoHolder(ImageView image) {
+            super(image);
+            mImage = image;
+        }
+
+        ImageView mImage;
     }
 }
---
+++ app/src/main/java/com/yinghuanhang/pdf/parser/MuPDFMainActivity.java
@@ -0,0 +1,17 @@
+package com.yinghuanhang.pdf.parser;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Cao-Human on 2018/1/2
+ */
+
+public class MuPDFMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mupdf_main);
+    }
+}
